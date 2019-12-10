ca5146bd942c36628b6aa978d5fe073da76a6989
José Manuel Moreira Vieira Gomes <jgomesmv@hotmail.com>
2019-12-10 14:42:34 +0000

    improve stories loading to read stories from diferent projects

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,4 @@
-import { configure } from '@storybook/angular';
+import { configure } from "@storybook/angular";
 
 // automatically import all files ending in *.stories.ts
-configure(require.context('../projects/shared-components/src/stories', true, /\.stories\.ts$/), module);
+configure(require.context("../projects/", true, /\.stories\.ts$/), module);
